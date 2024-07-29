b0b675a5caa412ab5a27597af1f4f9532a4a19f8
mg <mg@localhost>
2024-07-29 17:06:37 +0900

    main 2

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -4,6 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1112 dev main 1");
+		System.out.println("1112 dev main 1 2s");
 	}
 }
