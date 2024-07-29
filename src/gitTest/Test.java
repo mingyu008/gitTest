c7b964deb3546f766dfc93da95e78cac6c834468
mg <mg@localhost>
2024-07-29 17:35:57 +0900

    c 6

--- src/gitTest/Test.java
+++ src/gitTest/Test.java
@@ -4,6 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubssss
-		System.out.println("1112 dev main 1 2 3 4 5 dev 2 3 4");
+		System.out.println("1112 dev main 1 2 3 4 5 6 dev 2 3 4");
 	}
 }
